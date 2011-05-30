c15418875a26c70d7a059cebb54b997014577e2c
David Hu <davidhu91@gmail.com>
2011-05-30 00:09:23 -0400

    add more software that needs to be installed when setting up dev environment

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -7,3 +7,14 @@
 # - screen
 # - ack-grep
 # - autojump (write a script for this)
+# - google command line
+# - compiz with Grid (tiling by numpad)
+#   - sudo apt-get install compiz-fusion-plugins-extra
+# - keyboard shortcut to launch terminal window, maximized. start with screen.
+# - flux or Redshift for Linux: gtk-redshift -t 5700:3700
+# - Google Chrome w/ extensions: facebook photo zoom, google quick scroll,
+#     hover zoom (!!MUST), tweetdeck, pinned tabs (gmail, calendar, fb, reader,
+#     twitter, github, phone to chrome). Use Chrome sync.
+# - Ubuntu panels: cpu, memory, etc. and weather (set current location for time)
+# - make terminal and gvim windows semi-transparent
+# - copy ssh keys and run ssh-agent
